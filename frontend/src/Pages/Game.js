9ebc67c4f4ab15e2e44762fc8d64dd9533d06c5d
landonbu <brandonbl2021@gmail.com>
2021-12-30 22:14:03 -0500

    created game logic

--- frontend/src/Pages/Game.js
+++ frontend/src/Pages/Game.js
@@ -3,25 +3,14 @@ import {useState, useEffect, useCallback} from 'react';
 import Card from '../Components/Card';
 export default function Game(){
     const [deck, setDeck] = useState([])
-    const [balance, setBalance] = useState(100)
+    const [balance, setBalance] = useState(10000)
+    const [playerBet, setBet] = useState(0)
     const [player, setPlayer] = useState([])
     const [dealer, setDealer] = useState([])
-    /**
-    What I need:
-
-    a deck
-    a card
-
-    a shuffle function
-
-    a way to deal cards
-    a way for the dealer to play the game
-
-    the game logic itself
-
-    check for deck API or make own deck
-
-    */
+    const [playerValue, setPlayerValue] = useState(0)
+    const [dealerValue, setDealerValue] = useState(0)
+    const [hasAce, setAce] = useState(false)
+    const [dealerAce, setDealerAce] = useState(false)
 
     useEffect(()=>{
         fetch('https://deckofcardsapi.com/api/deck/new/draw/?count=52')
@@ -32,32 +21,101 @@ export default function Game(){
     },[])
     
     const deal = () => {
+        //Removes card from deck and sets player card deck + value
         setPlayer(player.concat(deck.slice(0,1)))
+        countPlayer(deck.slice(0,1)[0].value)
         setDeck(deck.slice(1,deck.length))
-        console.log(player)
+        
+        //Game logic?
+        if (playerValue > 21 && hasAce === false){
+            alert("You lost")
+            return;
+        }
+        if (playerValue > 21 && hasAce === true){
+            setPlayerValue(playerValue-10)
+            setAce(false)
+            return;
+        }
+        console.log(playerValue) 
+
     }
+
+    const play = () => {
+        if (dealerValue > 21){
+            alert("You won!")
+            return;
+        }
+        if (dealerValue >= playerValue){
+            alert("You lost")
+            return;
+        }
+        else{
+            alert("You won!")
+        }
+        
+    }
+
+    const countPlayer = (e) => {
+        console.log(e)
+        if (e === "KING" || e === "QUEEN" || e === "JACK"){
+            setPlayerValue(playerValue + 10)
+            return;
+        }
+        if (e === "ACE"){
+            setPlayerValue(playerValue + 11)
+            setAce(true)
+            return;
+        }
+        setPlayerValue(playerValue + parseInt(e))
+    }
+
+    const countDealer = (e) => {
+        console.log(e)
+        if (e === "KING" || e === "QUEEN" || e === "JACk"){
+            setDealerValue(playerValue + 10)
+            return;
+        }
+        if (e === "ACE"){
+            setDealerValue(playerValue + 11)
+            setDealerAce(true)
+        }
+        setPlayerValue(playerValue + parseInt(e))
+    }
+
+
+
+
 
     const bet = () => {
-        setBalance(balance+100)
-        console.log(balance)
+        //Find a better place to put this
+        if (!dealer.length){
+            setDealer(deck.slice(0,1))
+            countDealer(deck.slice(0,1)[0].value)
+            setDeck(deck.slice(1,deck.length))
+        }
+        if (balance < 100){
+            return;
+        }
+        setBet(playerBet+100)
+        setBalance(balance-100)
     }
-
-
-
 
     return(
     <div className='game--container'>
-        game
-        <button onClick={() => console.log(deck)}>this button is for when u place ur bet (maybe)</button>
-        <button onClick={deal}>
-            deal card
-        </button>
+        BALANCE:{balance}
 
-        {player.map(card => <Card image={card.image} value={card.value}/>)}
-
-        <button onClick={()=> console.log("player mapped:",player)}>
-            test
-        </button>
+        <button onClick={bet}>BET $100</button>
+        <button onClick={deal}>HIT</button>
+        <button onClick={play}>PLAY</button>
+        BET: {playerBet}
+        PLAYER  
+        <div className='game--cards'>
+            {player.map(card => <Card image={card.image} value={card.value}/>)}
+        </div>
+         DEALER
+        <div className='game--cards'>
+            {dealer.map(card => <Card image={card.image} value={card.value}/>)}
+        </div>
     
     </div>)
 }
