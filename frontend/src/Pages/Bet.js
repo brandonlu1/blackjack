b64e5f04ab21e069e4c0c76562808dcf38c2555c
landonbu <brandonbl2021@gmail.com>
2022-01-19 15:21:37 -0500

    slider in betting page

--- frontend/src/Pages/Bet.js
+++ frontend/src/Pages/Bet.js
@@ -3,9 +3,11 @@ import '../CSS/Assets/assets.css';
 import '../CSS/Pages/game.css';
 import {useState} from 'react';
 import {useNavigate} from 'react-router-dom';
+import {Slider, Box} from '@mui/material';
 
 export default function Bet(props){
     const [balance, setBalance] = useState(0)
+    const [ORIGINAL, setOriginal] = useState(0)
     const [playerBet, setBet] = useState(0)
     const [invalidBet, setInvalidBet] = useState(false)
     const [username, setUser] = useState(localStorage.getItem("user"))
@@ -16,27 +18,33 @@ export default function Bet(props){
             headers: {"Content-Type": "application/json",},
             body: JSON.stringify({username})})
         .then(res => res.json())
-        .then(response => { setBalance(response.balance)})
+        .then(response => { setBalance(response.balance)
+             setOriginal(response.balance)})
     },[])
+
+    const sliderBet = (e) => {
+        setBalance(ORIGINAL-e.target.value)
+        setBet(e.target.value)
+    }
     
-    const betPlus = () => {
+    const betPlus = (e) => {
         if (balance <= 0){
         setInvalidBet(true)
     }
         else{
-            setBet(playerBet+100)
-            setBalance(balance-100)
+            setBet(playerBet+e)
+            setBalance(balance-e)
             setInvalidBet(false)
         }
     }
 
-    const betMinus = () => {
+    const betMinus = (e) => {
         if (playerBet <=0){
         return
         }
         else{
-            setBet(playerBet-100)
-            setBalance(balance+100)
+            setBet(playerBet-e)
+            setBalance(balance+e)
         }
     }
 
@@ -73,14 +81,27 @@ export default function Bet(props){
 
     return(<div className='container'>
         <div className='title'>BETTING</div>
-        <div className='tag'>BALANCE:{balance}</div>
-        <div>
-            <button className="button--default" onClick={betPlus}>BET +100</button>
-            <button className="button--default" onClick={betMinus}>BET -100</button>
+        <div className='tag mt4'>BALANCE:{balance}</div>
+        <div className='tag'>BET: {playerBet}</div>
+        <Box sx={{width:'475px', marginBottom:'2vh'}}>
+        <Slider className="slider--style"
+        defaultValue={playerBet}
+        valueLabelDisplay="auto"
+        value={playerBet}
+        max={ORIGINAL}
+        color="primary"
+        onChange={(e)=>sliderBet(e)}
+        />
+        </Box>
+        <div className="mb8">
+        <button className="button--default" onClick={()=>betMinus(500)}>BET -500</button>
+        <button className="button--default" onClick={()=>betMinus(100)}>BET -100</button>
+            <button className="button--default" onClick={()=>betPlus(100)}>BET +100</button>
+            <button className="button--default" onClick={()=>betPlus(500)}>BET +500</button>
+
         </div>
         {invalidBet ? <p className="tag">Sorry, you do not have sufficient funds.</p>:null}
-        <div className='tag'>BET: {playerBet}</div>
-        <button className="button--default" onClick={submitBet} >PLAY</button>
+        <button className="button--big" onClick={submitBet} >PLAY</button>
 
 
 
